d4f59acce2766d60d64a2876f539b383ab571765
Ethan Brace <ethanabrace@gmail.com>
2019-08-16 14:02:00 -0400

    refactoring

--- src/EnvironmentSetCommandServiceProvider.php
+++ src/EnvironmentSetCommandServiceProvider.php
@@ -19,7 +19,7 @@ class EnvironmentSetCommandServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('command.env:set', EnvironmentSetCommand::class);
+        $this->app->bind('command.env:set', Environ::class);
 
         $this->commands([
             'command.env:set'
